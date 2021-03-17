aa004776ad05048786e9b3173affd7f1e18f97eb
Benjamin Wenger <ben@elixxir.ioo>
2021-03-17 16:00:49 -0700

    fixed all loading

--- main.go
+++ main.go
@@ -11,15 +11,19 @@ import (
 	"github.com/dtylman/gowd/bootstrap"
 )
 
-var password string
-var session string
-var ndfPath string
-var singleMngr *single.Manager
+var password = "password"
+var session = ".session"
+var ndfPath = "ndf.json"
+var botContactPath = "botContact.bin"
 var botContact contact.Contact
+
+var singleMngr *single.Manager
 
 var body *gowd.Element
 
 func main() {
+
+	botContact = readBotContact()
 
 	_, singleMngr = initClient()
 
